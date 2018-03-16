2dcf34154300d5d0c4496197e6448529b6457fd1
W4nn3s <Wannes@pottendeirm.be>
2018-03-16 13:10:18 +0100

    First

---
+++ Spielerei/Rijtjes.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Spielerei
+{
+    partial class Rijtjes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Rijtjes));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.picture6 = new System.Windows.Forms.PictureBox();
+            this.picture5 = new System.Windows.Forms.PictureBox();
+            this.picture4 = new System.Windows.Forms.PictureBox();
+            this.picture3 = new System.Windows.Forms.PictureBox();
+            this.picture2 = new System.Windows.Forms.PictureBox();
+            this.picture1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picture6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox4);
+            this.groupBox1.Controls.Add(this.pictureBox5);
+            this.groupBox1.Controls.Add(this.pictureBox6);
+            this.groupBox1.Controls.Add(this.pictureBox3);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(44, 43);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(295, 71);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "grpRijtjesLvl1";
+            this.groupBox1.Click += new System.EventHandler(this.groupBox1_Click);
+            // 
+            // picture6
+            // 
+            this.picture6.Location = new System.Drawing.Point(711, 61);
+            this.picture6.Name = "picture6";
+            this.picture6.Size = new System.Drawing.Size(116, 116);
+            this.picture6.TabIndex = 6;
+            this.picture6.TabStop = false;
+            this.picture6.Click += new System.EventHandler(this.picture6_Click);
+            // 
+            // picture5
+            // 
+            this.picture5.Location = new System.Drawing.Point(589, 61);
+            this.picture5.Name = "picture5";
+            this.picture5.Size = new System.Drawing.Size(116, 116);
+            this.picture5.TabIndex = 5;
+            this.picture5.TabStop = false;
+            this.picture5.Click += new System.EventHandler(this.picture5_Click);
+            // 
+            // picture4
+            // 
+            this.picture4.Location = new System.Drawing.Point(467, 61);
+            this.picture4.Name = "picture4";
+            this.picture4.Size = new System.Drawing.Size(116, 116);
+            this.picture4.TabIndex = 4;
+            this.picture4.TabStop = false;
+            this.picture4.Click += new System.EventHandler(this.picture4_Click);
+            // 
+            // picture3
+            // 
+            this.picture3.Location = new System.Drawing.Point(345, 61);
+            this.picture3.Name = "picture3";
+            this.picture3.Size = new System.Drawing.Size(116, 116);
+            this.picture3.TabIndex = 3;
+            this.picture3.TabStop = false;
+            this.picture3.Click += new System.EventHandler(this.picture3_Click);
+            // 
+            // picture2
+            // 
+            this.picture2.Location = new System.Drawing.Point(223, 61);
+            this.picture2.Name = "picture2";
+            this.picture2.Size = new System.Drawing.Size(116, 116);
+            this.picture2.TabIndex = 2;
+            this.picture2.TabStop = false;
+            this.picture2.Click += new System.EventHandler(this.picture2_Click);
+            // 
+            // picture1
+            // 
+            this.picture1.InitialImage = null;
+            this.picture1.Location = new System.Drawing.Point(101, 62);
+            this.picture1.Name = "picture1";
+            this.picture1.Size = new System.Drawing.Size(116, 116);
+            this.picture1.TabIndex = 1;
+            this.picture1.TabStop = false;
+            this.picture1.Click += new System.EventHandler(this.picture1_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(246, 18);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(45, 42);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(200, 18);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(45, 42);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(154, 18);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(45, 42);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 3;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(108, 18);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(45, 42);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(62, 18);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(45, 42);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(16, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(45, 42);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Rijtjes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 261);
+            this.Controls.Add(this.picture6);
+            this.Controls.Add(this.picture5);
+            this.Controls.Add(this.picture4);
+            this.Controls.Add(this.picture3);
+            this.Controls.Add(this.picture2);
+            this.Controls.Add(this.picture1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Rijtjes";
+            this.Text = "Rijtjes";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picture6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox picture1;
+        private System.Windows.Forms.PictureBox picture2;
+        private System.Windows.Forms.PictureBox picture3;
+        private System.Windows.Forms.PictureBox picture4;
+        private System.Windows.Forms.PictureBox picture5;
+        private System.Windows.Forms.PictureBox picture6;
+    }
+}
---
+++ Spielerei/Rijtjes.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Windows.Forms;
+
+namespace Spielerei
+{
+    public partial class Rijtjes : Form
+    {
+
+        public int ronde = 0;
+        public int maxrondes = 3;
+        List<Ronde> rondes = new List<Ronde>();
+
+
+        public class Ronde
+        {
+            public Bitmap pic1src;
+            public Bitmap pic2src;
+            public Bitmap pic3src;
+            public Bitmap pic4src;
+            public Bitmap pic5src;
+            public Bitmap pic6src;
+            public int juisteantwoord;
+
+            public Ronde(Bitmap pic1src, Bitmap pic2src, Bitmap pic3src, Bitmap pic4src, Bitmap pic5src, Bitmap pic6src, int juisteantwoord)
+            {
+                this.pic1src = pic1src;
+                this.pic2src = pic2src;
+                this.pic3src = pic3src;
+                this.pic4src = pic4src;
+                this.pic5src = pic5src;
+                this.pic6src = pic6src;
+                this.juisteantwoord = juisteantwoord;
+            }
+
+        }
+        public Rijtjes()
+        {
+            InitializeComponent();
+            
+
+            
+
+                rondes.Add(new Ronde(
+                Spielerei.Properties.Resources._116citroen,
+                Spielerei.Properties.Resources._116peer,
+                Spielerei.Properties.Resources._116blokken,
+                Spielerei.Properties.Resources._116kiwi,
+                Spielerei.Properties.Resources._116sinaasappel,
+                Spielerei.Properties.Resources._116ananas,
+                3));
+
+                rondes.Add(new Ronde(
+                Spielerei.Properties.Resources._116ezel,
+                Spielerei.Properties.Resources._116krab,
+                Spielerei.Properties.Resources._116hond,
+                Spielerei.Properties.Resources._116kip,
+                Spielerei.Properties.Resources._116peer,
+                Spielerei.Properties.Resources._116onijn,
+                5));
+
+
+                rondes.Add(new Ronde(
+                Spielerei.Properties.Resources._116frietjes,
+                Spielerei.Properties.Resources._116brood,
+                Spielerei.Properties.Resources._116pizza,
+                Spielerei.Properties.Resources._116vlinder,
+                Spielerei.Properties.Resources._116kaas,
+                Spielerei.Properties.Resources._116spaghetti,
+                4));
+
+                rondes.Add(new Ronde(
+                Spielerei.Properties.Resources._116puzzel,
+                Spielerei.Properties.Resources._116teddybeer,
+                Spielerei.Properties.Resources._116pop,
+                Spielerei.Properties.Resources._116ballon,
+                Spielerei.Properties.Resources._116hobbelpaar,
+                Spielerei.Properties.Resources._116peer,
+                6));
+
+
+
+
+            picture1.Visible = false;
+            picture2.Visible = false;
+            picture3.Visible = false;
+            picture4.Visible = false;
+            picture5.Visible = false;
+            picture6.Visible = false;
+
+            picture1.Enabled = false;
+            picture2.Enabled = false;
+            picture3.Enabled = false;
+            picture4.Enabled = false;
+            picture5.Enabled = false;
+            picture6.Enabled = false;
+
+            picture1.SizeMode = PictureBoxSizeMode.StretchImage;
+            picture2.SizeMode = PictureBoxSizeMode.StretchImage;
+            picture3.SizeMode = PictureBoxSizeMode.StretchImage;
+            picture4.SizeMode = PictureBoxSizeMode.StretchImage;
+            picture5.SizeMode = PictureBoxSizeMode.StretchImage;
+            picture6.SizeMode = PictureBoxSizeMode.StretchImage;
+
+
+        }
+
+        private void groupBox1_Click(object sender, EventArgs e)
+        {
+            StartSpel();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            StartSpel();
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            StartSpel();
+        }
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            StartSpel();
+        }
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            StartSpel();
+        }
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            StartSpel();
+        }
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            StartSpel();
+        }
+
+        private void StartSpel()
+        {
+            groupBox1.Visible = false;
+            groupBox1.Enabled = false;
+
+            picture1.Visible = true;
+            picture2.Visible = true;
+            picture3.Visible = true;
+            picture4.Visible = true;
+            picture5.Visible = true;
+            picture6.Visible = true;
+
+            picture1.Enabled = true;
+            picture2.Enabled = true;
+            picture3.Enabled = true;
+            picture4.Enabled = true;
+            picture5.Enabled = true;
+            picture6.Enabled = true;
+
+
+            TekenRonde(ronde);
+
+        }
+
+        private void StopSpel()
+        {
+            groupBox1.Visible = true;
+            groupBox1.Enabled = true;
+
+            picture1.Visible = false;
+            picture2.Visible = false;
+            picture3.Visible = false;
+            picture4.Visible = false;
+            picture5.Visible = false;
+            picture6.Visible = false;
+
+            picture1.Enabled = false;
+            picture2.Enabled = false;
+            picture3.Enabled = false;
+            picture4.Enabled = false;
+            picture5.Enabled = false;
+            picture6.Enabled = false;
+
+        }
+
+        public void TekenRonde(int ronde)
+        {
+            picture1.Image = rondes[ronde].pic1src;
+            picture2.Image = rondes[ronde].pic2src;
+            picture3.Image = rondes[ronde].pic3src;
+            picture4.Image = rondes[ronde].pic4src;
+            picture5.Image = rondes[ronde].pic5src;
+            picture6.Image = rondes[ronde].pic6src;
+
+        }
+
+        private void picture1_Click(object sender, EventArgs e)
+        {
+            Valideren(1);
+        }
+
+        private void picture2_Click(object sender, EventArgs e)
+        {
+            Valideren(2);
+        }
+
+        private void picture3_Click(object sender, EventArgs e)
+        {
+            Valideren(3);
+        }
+
+        private void picture4_Click(object sender, EventArgs e)
+        {
+            Valideren(4);
+        }
+
+        private void picture5_Click(object sender, EventArgs e)
+        {
+            Valideren(5);
+        }
+
+        private void picture6_Click(object sender, EventArgs e)
+        {
+            Valideren(6);
+        }
+
+
+        private void Valideren(int keuze)
+        {
+            if (keuze == rondes[ronde].juisteantwoord)
+            {
+                SoundPlayer correct = new SoundPlayer(Spielerei.Properties.Resources.correct);
+                correct.Play();
+                ronde++;
+                if (ronde <= maxrondes)
+                {
+                    TekenRonde(ronde);
+                }
+                else
+                {
+                    SoundPlayer applaus = new SoundPlayer(Spielerei.Properties.Resources.applaus);
+                    applaus.Play();
+
+                    StopSpel();
+                }
+            
+            }
+            else
+            {
+                SoundPlayer fout = new SoundPlayer(Spielerei.Properties.Resources.wrong);
+                fout.Play();
+            }
+
+
+        }
+    }
+}
